23da0e9571d18ccb9e4149a0e677381ba6f71c05
Shirazzzzz <97777320+Shirazzzzz@users.noreply.github.com>
2024-10-09 16:45:29 +0530

    Update Playing with Data 2.py

--- Playing with Data 2.py
+++ Playing with Data 2.py
@@ -1,14 +1,8 @@
-import plotly.express as px
-import pandas as pd
-from plotly.offline import plot
-
-db = pd.read_csv("cab_data_2.csv")
-print(db.head())  # Img_12
-
+##RESEARCH COLLABORATORS & RECRUITERS CAN MAIL ME AT ssz.cprog@gmail.com FOR ALL 20 CODE FILES, SETUP & DOCUMENTATION
 
 box = px.box(db, x='dispatching_base_number', y='active_vehicles', title='Box Plot')
-plot(box, filename='db_box_plot.html')  # Img_13
+plot(box, filename='db_box_plot.html')  # Img_14.0
 
 
 violin = px.violin(db, x='dispatching_base_number', y='active_vehicles', title='Violin Plot')
-plot(violin, filename='db_violin_plot.html')  # Img_14
+plot(violin, filename='db_violin_plot.html')  # Img_14.1
